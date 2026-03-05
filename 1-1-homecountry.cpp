4f55d5b77b3cc21f90b7f2c15ee9bf5ccd2604b5
Anna Kim <flyjihye@gmail.com>
2026-03-05 17:21:39 +0900

    add chap1 files

---
+++ 1-1-homecountry.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+	std::cout << "나의 조국은 대한민국입니다.\n";
+	return 0;
+}
---
+++ 1-3-remainder.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	int n = 15;
+	int m = 4;
+	int remainder = n % m;
+
+	std::cout << n << "를 " << m << "로 나눈 나머지는 " << remainder << "입니다.\n";
+	return 0;
+}
