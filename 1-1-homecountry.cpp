322e906e1dda4aa462f8c81aa7bf8c56bfd50b8e
Anna Kim <flyjihye@gmail.com>
2026-03-08 15:22:54 +0900

    delete

--- 1-1-homecountry.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-	std::cout << "나의 조국은 대한민국입니다.\n";
-	return 0;
-}
--- 1-2-hobby.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-	std::cout << "나의 이름은 황기태\n";
-	std::cout << "나의 전공은 컴퓨터공학\n";
-	std::cout << "나의 취미는 테니스입니다.\n";
-	return 0;
-}
